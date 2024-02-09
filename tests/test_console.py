99b10a8ef25d6ec353acfcb6b2c00678197c93f3
Mugara374 <m.mushi@alustudent.com>
2024-02-09 17:20:12 +0200

    test console

--- tests/test_console.py
+++ tests/test_console.py
@@ -4,7 +4,6 @@ Contains the class TestConsoleDocs
 """
 
 import console
-import inspect
 import pep8
 import unittest
 HBNBCommand = console.HBNBCommand
@@ -28,14 +27,14 @@ class TestConsoleDocs(unittest.TestCase):
 
     def test_console_module_docstring(self):
         """Test for the console.py module docstring"""
-        self.assertIsNot(console.__doc__, None,
-                         "console.py needs a docstring")
+        self.assertIsNotNone(console.__doc__,
+                             "console.py needs a docstring")
         self.assertTrue(len(console.__doc__) >= 1,
                         "console.py needs a docstring")
 
     def test_HBNBCommand_class_docstring(self):
         """Test for the HBNBCommand class docstring"""
-        self.assertIsNot(HBNBCommand.__doc__, None,
-                         "HBNBCommand class needs a docstring")
+        self.assertIsNotNone(HBNBCommand.__doc__,
+                             "HBNBCommand class needs a docstring")
         self.assertTrue(len(HBNBCommand.__doc__) >= 1,
                         "HBNBCommand class needs a docstring")
